489d6bbca0f95f1c6e8fb391c08c050ec5d4d3c7
Bert Riffelmacher <sotsoguk@gmail.com>
2019-10-22 01:48:12 +0200

    [Fireworks] First Explosion

--- fireworks/sketch.js
+++ fireworks/sketch.js
@@ -7,28 +7,53 @@ function setup() {
 
 function draw() {
   // put drawing code here
-  background(0);
-  r.run();
+  background(0,100);
+  if (!r.finished)
+    r.run();
   
 }
 
 class Rocket{
 
   constructor(col, pos){
+    this.maxLifetime = 300;
     this.col = col;
     this.pos = pos;
     this.vel = createVector(0.1,-8);
     this.grav = createVector(0,0.01);
     this.timeToExplode = 50;
+    this.timeToVanish = this.maxLifetime;
+    this.finished = false;
+    this.glitter =[];
+    this.numGlitter = 200;
   }
 
   update(){
+    if (this.timeToVanish <=0){
+      this.finished = true;
+      return;
+    }
     if (this.timeToExplode >0){
       this.pos.add(this.vel);
       this.vel.add(this.grav);
       this.timeToExplode--;
     }
+    else {
 
+      if (this.timeToVanish == this.maxLifetime){
+        for (let i = 0; i < this.numGlitter; i++){
+          this.glitter.push(new Particle(createVector(this.pos.x+random(10),this.pos.y+random(10)),p5.Vector.random2D().setMag(random(0.2,0.8))));
+         
+        }
+      }
+      for (let i=0; i < this.glitter.length;++i){
+        this.glitter[i].applyForce(createVector(0,0.01));
+        this.glitter[i].update();
+        console.log(this.glitter[i].pos.x);
+      }
+      this.timeToVanish--;
+    }
+    
   }
   draw(){
     if (this.timeToExplode > 0){
@@ -36,12 +61,44 @@ class Rocket{
       fill(255,this.timeToExplode*5);
       ellipse(this.pos.x, this.pos.y,2,8);
     }
-    else{
-      fill(255,0,0);
-      ellipse(this.pos.x,this.pos.y,50,50);
+    else {
+      noStroke();
+      fill(color(255,0,0));
+      for (let i=0;i<this.glitter.length;++i){
+        this.glitter[i].draw();
+        // console.log(i);
+      }
+      
     }
   }
 
+  run(){
+    this.update();
+    this.draw();
+  }
+}
+
+class Particle{
+  constructor(posVec, velVec){
+    this.pos = posVec;
+    this.vel = velVec;
+    this.acc = createVector(float(0), float(0));
+  }
+
+  applyForce(forceVec){
+    this.acc.add(forceVec);
+  }
+  update(){
+    this.pos.add(this.vel);
+    this.vel.add(this.acc);
+    this.acc.mult(0);
+  }
+
+  draw(){
+    noStroke();
+    //fill(255);
+    ellipse(this.pos.x, this.pos.y, 3,3);
+  }
   run(){
     this.update();
     this.draw();
