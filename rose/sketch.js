2954bc2bcc5486b0ada8e1e7a03daffb8fb98ec5
Bert Riffelmacher <sotsoguk@gmail.com>
2019-10-27 02:14:52 +0200

    [Rose] Sliders added

--- rose/sketch.js
+++ rose/sketch.js
@@ -1,82 +1,133 @@
 let nSlider, dSlider, fillCheckbox;
-let fillStyle = true;
+let rainbow = true;
 let saveButton;
-let d = 5;
 let n = 4;
-let c = 0;
-let r = 250;
+let d = 1;
+let col;
+let bcol;
+let r = 500;
+let w = 2;
+let alpha = 100;
 let offset = 0;
+let xOffset = 0;
+let yOffset = 0;
 let angleDelta = 0.01;
 let stepDelta = 20;
+let ctx;
 function setup() {
   // put setup code here
-  createCanvas(600, 600);
+  ctx = createCanvas(1200, 1200);
   background(0);
-  dSlider = createSlider(1,50,5);
-  dSlider.position(20,height+20);
-  dSlider.changed(redraw);
-  nSlider = createSlider(1,50,4);
-  nSlider.position(20,height+50);
+  fill(0);
+  text("n = ",0,height+100);
+  nSlider = createSlider(1, 50, n);
+  nSlider.position(20, height + 20);
   nSlider.changed(redraw);
+  colorPick = createColorPicker(color('white'));
+   colorPick.position(160,height+10);
+  // colorPick.changed(redraw);
+  colorPick.changed(redraw);
+  bcolorPick = createColorPicker(color('black'));
+  bcolorPick.position(210,height+10);
+  bcolorPick.changed(redraw);
+  dSlider = createSlider(1, 50, d);
+  dSlider.position(20, height + 50);
+  dSlider.changed(redraw);
+  wSlider = createSlider(1,20,w);
+  wSlider.position(20,height + 80);
+  wSlider.changed(redraw);
+  rSlider = createSlider(50,height,r);
+  rSlider.position(20,height + 130);
+  rSlider.changed(redraw);
+  xSlider = createSlider(-width/2,width/2,xOffset);
+  xSlider.position(20,height + 150);
+  xSlider.changed(redraw);
+  ySlider = createSlider(-height/2,height/2,yOffset);
+  ySlider.position(20,height + 170);
+  ySlider.changed(redraw);
+  alphaSlider = createSlider(1,100,alpha);
+  alphaSlider.position(20, height + 110);
+  alphaSlider.changed(redraw);
   saveButton = createButton('save');
-  saveButton.position(200,height+20);
+  saveButton.position(280, height + 20);
   saveButton.mousePressed(saveImage);
-  fillCheckbox = createCheckbox('fill', true);
-  fillCheckbox.changed(redraw);
+  rainbowCheckbox = createCheckbox('rainbow', true);
+  rainbowCheckbox.changed(redraw);
 }
 
 function draw() {
   // put drawing code here
-  
+
+  drawRose(ctx);
+  // console.log("Total:" + totalSteps);
+  // console.log("Done:" + steps);
+  //offset +=.005;
+  noLoop();
+}
+
+function saveImage() {
+  save('rose.png');
+}
+
+function drawRose()
+{
   d = dSlider.value();
   n = nSlider.value();
-  fillStyle = fillCheckbox.value();
-  console.log(fillStyle);
-  background(0,30);
+  w = wSlider.value();
+  r = rSlider.value();
+  xOffset = xSlider.value();
+  yOffset = ySlider.value();
+  col = colorPick.color();
+  bcol = bcolorPick.color();
+  alpha = alphaSlider.value();
+  rainbow = rainbowCheckbox.checked();
+  col.setAlpha(alpha*2.55);
+  // console.log(fillStyle);
+  background(bcol);
   // fill(255);
   noStroke();
   fill(255);
-  text("k="+d+"/"+n, 20, height-30);
+  text("k=" + n + "/" + d, 20, height - 30);
   colorMode(HSB);
   angle = 0;
-  let k = d / n;
+  let k = n / d;
   //fill(181,100);
   // noFill();
   //stroke(199,200);
-  stroke(0,100,100,0.5);
-  strokeWeight(15);
-  translate(width/2,height/2);
+  stroke(0, 100, 100, 0.5);
+  strokeWeight(w);
+  translate(width / 2, height / 2);
   //rotate(offset);
   let steps = 0;
-  let totalSteps = 2*PI*n/angleDelta;
+  let totalSteps = ceil(2 * PI * d / angleDelta);
   // beginShape();
-  while (angle <= n*PI*2+angleDelta){
-    if (steps%stepDelta === 0){
-      stroke( steps*360/totalSteps,100,100,1);
+  while (angle <= (d * PI * 2 + angleDelta)) {
+    if (steps % stepDelta === 0) {
+      if (rainbow){
+        stroke(floor(steps * 360 / totalSteps), 100, 100, alpha / 100);
+      }
+      else{
+        
+        stroke(col);
+      }
       //  if (!fillStyle == true){
-        //fill( steps*360/totalSteps,100,100,1);
+      //fill( steps*360/totalSteps,100,100,1);
       // }
       // else
-       noFill();
+      noFill();
       beginShape();
       if (steps != 0)
-        vertex(cos(k*(angle-angleDelta))*cos((angle-angleDelta))*r+c,
-        cos(k*(angle-angleDelta))*sin((angle-angleDelta))*r+c);
+        vertex(cos(k * (angle - angleDelta)) * cos((angle - angleDelta)) * r +xOffset,
+          cos(k * (angle - angleDelta)) * sin((angle - angleDelta)) * r +yOffset);
     }
-    let x = cos(k*angle)*cos(angle)*r+c;
-    let y = cos(k*angle)*sin(angle)*r+c;
-    vertex(x,y);
-    if (steps %20 == 19){
+    let x = cos(k * angle) * cos(angle) * r+xOffset;
+    let y = cos(k * angle) * sin(angle) * r+yOffset;
+    vertex(x, y);
+    if (steps % 20 == 19) {
       endShape();
     }
     angle += angleDelta;
     steps++;
   }
   endShape();
-  //offset +=.005;
-  noLoop();
-}
-
-function saveImage(){
-  save('rose.png');
 }
